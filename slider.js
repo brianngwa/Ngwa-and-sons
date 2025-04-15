7156362190a965b14d98a6dcb821a38ec1978a45
Brian Ngwa <briano99ngwa@gmail.com>
2025-04-15 20:52:09 +0100

    Slider animation on images

---
+++ slider.js
@@ -0,0 +1,35 @@
+let currentIndex = {
+  slider1: 0,
+  slider2: 0,
+};
+
+function showSlide(sliderId, index) {
+  const slider = document.getElementById(sliderId);
+  const slides = slider.querySelector(".slides");
+  const totalSlides = slides.children.length;
+
+  if (index >= totalSlides) {
+    currentIndex[sliderId] = 0;
+  } else if (index < 0) {
+    currentIndex[sliderId] = totalSlides - 1;
+  } else {
+    currentIndex[sliderId] = index;
+  }
+
+  const offset = -currentIndex[sliderId] * 100;
+  slides.style.transform = `translateX(${offset}%)`;
+}
+
+function nextSlide(sliderId) {
+  showSlide(sliderId, currentIndex[sliderId] + 1);
+}
+
+function prevSlide(sliderId) {
+  showSlide(sliderId, currentIndex[sliderId] - 1);
+}
+
+// Initialize sliders
+showSlide("slider1", 0);
+showSlide("slider2", 0);
+showSlide("slider3", 0);
+showSlide("slider4", 0);
